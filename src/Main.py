3cecd5eedf7f84a5ded7986f5bc1e070f69a477f
unknown <yangboz@yzhou24.asiapacific.hpqcorp.net>
2013-05-14 15:21:17 +0800

    ADDED:Main.py with example source code;

---
+++ src/Main.py
@@ -0,0 +1,15 @@
+# Reverse Cipher
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+message = 'There can be secret,if two of them are dead.'
+
+translated = ''
+
+i = len(message)-1
+
+while i>=0:
+    translated = translated + message[i]
+    i = i - 1
+
+print(translated)    
+    
